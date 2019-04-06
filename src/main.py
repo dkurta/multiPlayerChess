bd98d27f8a876916cac2cbbef1e1a6bc6e92d9e1
dkurta <s76389@beuth-hochschule.de>
2019-04-07 00:54:58 +0200

    play method in model refactoring in main

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import chess
 import chess.engine
 from src import model
 
-# engine = chess.engine.SimpleEngine.popen_uci('/usr/games/stockfish')
-#
+engine = chess.engine.SimpleEngine.popen_uci('/usr/games/stockfish')
+
 # board = chess.Board()
 # info = engine.analyse(board, chess.engine.Limit(time=4.100))
 # print(info)
@@ -18,3 +18,4 @@ from src import model
 
 if __name__ == '__main__':
     model = model.Model()
+    model.play()
--- src/model.py
+++ src/model.py
@@ -1,12 +1,38 @@
 import chess
 
+ENGINE_PATH = '/usr/games/stockfish'
+
 
 class Model:
     def __init__(self):
         self.board = chess.Board()
-        self.numPlayers = int(input("Anzahl Spieler eingeben"))
+        self.num_players = int(input("Anzahl Spieler eingeben"))
         self.players = list()
-        for x in range(self.numPlayers):
+        for x in range(self.num_players):
             name = input("Namen des {}. Spielers eingeben".format(x+1))
             self.players.append((name, 0))
-        print(self.players)
+        self.engine = chess.engine.SimpleEngine.popen_uci(ENGINE_PATH)
+        self.halfmove_number = 0
+        self.update_halfmove_number()
+
+    def update_halfmove_number(self):
+        turn = self.board.turn
+        fullmoves = self.board.fullmove_number
+        halfmoves = (fullmoves-1)*2             # -1 because fullmoves has offset + 1
+        if not turn:
+            # black to move
+            halfmoves += 1
+        self.halfmove_number = halfmoves
+
+    def play(self):
+        while not self.board.is_game_over():
+            player_to_move = self.halfmove_number % self.num_players
+            valid_move_inserted = False
+
+            while not valid_move_inserted:
+                move = input("{} am Zug. Bitte Zug eingeben.".format(self.players[player_to_move][0]))
+                if move in list(map(lambda m: self.board.san(m), list(self.board.legal_moves))):
+                    self.board.push_san(move)
+                    valid_move_inserted = True
+            print(self.board)
+            self.update_halfmove_number()
