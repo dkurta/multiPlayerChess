25e6614bc9306071da6884c914d2b8e206d472f1
dkurta <s76389@beuth-hochschule.de>
2019-04-08 12:24:56 +0200

    calculate delta in new method

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from src import model
 
 engine = chess.engine.SimpleEngine.popen_uci('/usr/games/stockfish')
 
-# board = chess.Board()
-# info = engine.analyse(board, chess.engine.Limit(time=4.100))
-# print(info)
-# print("score= ", info['score'])
-# print(type(info['score']))
+board = chess.Board()
+info = engine.analyse(board, chess.engine.Limit(time=0.100))
+print(info)
+print("score= ", info['score'])
+print(type(info['score']))
 
 
 
--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 import chess
 
 ENGINE_PATH = '/usr/games/stockfish'
-
+ENGINE_TIME_LIMIT = 0.100
 
 class Model:
     def __init__(self):
@@ -26,14 +26,31 @@ class Model:
 
     def play(self):
         while not self.board.is_game_over():
+            pre_rating = self.engine.analyse(self.board, chess.engine.Limit(time=ENGINE_TIME_LIMIT))
 
             player_to_move = self.halfmove_number % self.num_players
+            move_for_white_side = self.board.turn
             valid_move_inserted = False
-
             while not valid_move_inserted:
                 move = input("{} am Zug. Bitte Zug eingeben.".format(self.players[player_to_move][0]))
                 if move in list(map(lambda m: self.board.san(m), list(self.board.legal_moves))):
                     self.board.push_san(move)
                     valid_move_inserted = True
+
+            new_rating = self.engine.analyse(self.board, chess.engine.Limit(time=ENGINE_TIME_LIMIT))
+            delta = calculate_delta(move_for_white_side, pre_rating['score'], new_rating['score'])
+            pre_rating = new_rating
+            self.players[player_to_move][1] += delta
             print(self.board)
+            print(self.players)
             self.update_halfmove_number()
+
+
+def calculate_delta(color, rating_before, rating_after):
+    print(rating_before, rating_after)
+    delta = rating_after - rating_before
+    if not color:
+        # moved for black
+        print("Moved for black")
+        delta = delta*(-1)
+    return delta
